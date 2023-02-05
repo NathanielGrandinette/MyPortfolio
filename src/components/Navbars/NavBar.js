305c4dec069efcf160ece65d47032a4a9797c89f
Nathaniel Grandinette <nathaniel.grandinette@gmail.com>
2023-02-04 20:40:28 -0700

    Navbar changes

--- src/components/Navbars/NavBar.js
+++ src/components/Navbars/NavBar.js
@@ -1,49 +1,40 @@
-import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
+import { Navbar, Container, Nav, NavDropdown, Popover } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { Spin as Hamburger } from 'hamburger-react'
 import './NavBar.css'
 
 const NavBar = () => {
-
+    const [toggle, setToggle] = useState(false)
 
     return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar collapseOnSelect className="nav" expand="lg" bg="dark" variant="dark">
             <Container>
                 <Navbar.Brand>
+                    Welcome
+                </Navbar.Brand>
+                <Nav >
+                    <Hamburger color="white" onToggle={setToggle} />
+                </Nav>
+            </Container>
+            {toggle ?
+                <Container className='links'>
                     <Link to='/' style={{ textDecoration: 'none', color: 'white' }}>
                         Home
                     </Link>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                    </Nav>
-                    <Nav>
-                        <NavDropdown className='profileImage' title={<img src='profile-photo.jpg' alt='' />} id="collasible-nav-dropdown">
-                            <NavDropdown.Item>
-                                <Link to='/resume' className='navLink'>
-                                    Resume
-                                </Link>
-                            </NavDropdown.Item>
-                            <NavDropdown.Item >
-                                <Link to='/projects' className='navLink'>
-                                    Projects
-                                </Link>
-                            </NavDropdown.Item>
-                            <NavDropdown.Item>
-                                <Link to='/about' className='navLink'>
-                                    About Me
-                                </Link>
-                            </NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item>
-                                <Link to='/contact' className='navLink'>
-                                    Contact Me
-                                </Link>
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
+                    <Link to='/resume' className="navLink" >
+                        Resume
+                    </Link>
+                    <Link to='/projects' className='navLink'>
+                        Projects
+                    </Link>
+                    <Link to='/about' className='navLink'>
+                        About Me
+                    </Link>
+                    <Link to='/contact' className='navLink'>
+                        Contact Me
+                    </Link>
+                </Container> : ''}
         </Navbar>
     )
 }
