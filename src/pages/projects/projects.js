917ff76fc806e9d1d81af77e3e45b7f1d2808208
Nathaniel Grandinette <nathaniel.grandinette@gmail.com>
2023-02-06 19:10:54 -0700

    Added project

--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -1,21 +1,23 @@
 import './projects.css'
 
 const Projects = () => {
-
-    /* <span style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-                 <div class="polaroid rotate_right">
-                     <img src="pulpitrock.jpg" alt="Pulpit rock" width="284" height="213" />
-                     <p class="caption">The pulpit rock in Lysefjorden, Norway.</p>
-                 </div>
- 
-                 <div class="polaroid rotate_left">
-                     <img src="cinqueterre.jpg" alt="Monterosso al Mare" width="284" height="213" />
-                     <p class="caption">Monterosso al Mare. One of the five villages in Cinque Terre, Italy.</p>
-                 </div>
-             </span>*/
+    /**/
     return (
-        <div style={{ margin: '0 auto', height: '100%' }}>
-            <h1 style={{ height: '100%' }}>...Coming <span style={{ color: '#3498db' }}>soon</span></h1>
+        <div>
+            <div style={{ margin: '50px auto' }}>
+                <h1 style={{ height: '100%' }}>My <span style={{ color: '#3498db' }}>Projects</span></h1>
+            </div>
+            <span style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '75vh' }}>
+                <div class="polaroid rotate_right">
+                    <img src="chatbot.jpg" alt="Pulpit rock" width="250" height="213" />
+                    <p class="caption">This project builds a conversational interface using React, where users can
+                        input text and receive instant responses. The goal is to improve the user experience through efficient support via a conversational interface.
+                    </p>
+                </div>
+
+            </span>
+
+
         </div>
     )
 }
