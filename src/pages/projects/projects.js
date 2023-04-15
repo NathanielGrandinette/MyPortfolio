73b9c18067375b96d5cc2519844aea5cfc02388e
Nathaniel Grandinette <nathaniel.grandinette@gmail.com>
2023-04-14 18:16:02 -0700

    Added portfolio links

--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -8,34 +8,44 @@ const Projects = () => {
             image: 'connect4.png',
             description: "This project is a classic Connect 4 game built entirely using vanilla JavaScript. The game features a simple and intuitive user interface that allows players to quickly and easily place their pieces on the board and compete against each other. Building this game required a deep understanding of JavaScript programming concepts such as variables, functions, and loops. We also had to develop advanced logic to detect when a player has won, as well as to prevent illegal moves such as placing pieces in already filled columns. ",
             date: "October 2022",
+            github: 'https://github.com/KenzieAcademy-Students/teamwork-assessment-connect-four-NathanielGrandinette',
         },
         {
             name: "ChatBot",
             image: 'chatbot.jpg',
             description: "This project builds a conversational interface using React, where users can input text and receive instant responses. The goal is to improve the user experience through efficient support via a conversational interface.",
-            date: "January 2023",
+            date: "January 2023"
         },
         {
             name: "Productivity App",
             image: 'productivity.jpg',
             description: "This project is focused on developing a todo list app that provides a streamlined and intuitive interface for managing tasks and staying organized. Using React, we have built a powerful and user-friendly app that allows users to easily create and manage multiple todo lists, add and complete tasks, and set reminders to stay on top of deadlines. The goal of this project is to provide a better user experience by making it easy to stay organized and on top of tasks. Rather than relying on traditional pen and paper lists or complex project management software, our app provides a simple and efficient solution that can be accessed from anywhere.",
             date: "March 2023",
+            github: 'https://github.com/TimothyDanfield/Agenda-X-Productivity',
+            live: 'https://agendaxproductivity.herokuapp.com/'
         },
     ]
     return (
         <div className="timeline">
-            <h1 style={{marginTop: 50}}>MY <span className="about-me-header-name">PROJECTS</span></h1>
+            <h1 style={{ marginTop: 50 }}>MY <span className="about-me-header-name">PROJECTS</span></h1>
             {events.map((event, index) => (
                 <div key={event.id} className='timeline-event'>
-                    <div className="timeline-event-content">
-                        <div className="timeline-event-image">
-                            <img src={event.image} alt={event.name} style={{ height: 250, width: 250 }} />
+                    <div className='timeline-events'>
+                        <div className="timeline-event-content">
+                            <div className="timeline-event-image">
+                                <img src={event.image} alt={event.name} style={{ height: 250, width: 250 }} />
+                            </div>
+                            <div>
+                                <h2>{event.name}</h2>
+                                <p style={{ textAlign: 'justify', textJustify: 'inter-word', width: '75%', margin: '0 auto' }}>{event.description}</p>
+                            </div>
+                            <div className="timeline-event-date">{event.date}</div>
+
                         </div>
-                        <div>
-                            <h2>{event.name}</h2>
-                            <p style={{textAlign: 'justify', textJustify: 'inter-word', width: '75%', margin: '0 auto'}}>{event.description}</p>
+                        <div className='project-links'>
+                            {event.github ? <a href={event.github} target="_blank">Github</a> : '' }
+                            {event.live ? <a href={event.live} target="_blank">Live</a> : ''}
                         </div>
-                        <div className="timeline-event-date">{event.date}</div>
                     </div>
                 </div>
             ))}
