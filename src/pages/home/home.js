e4b09c8140fc13738488e1be09b40c43acb56bfd
Nathaniel Grandinette <nathaniel.grandinette@gmail.com>
2023-06-13 16:02:21 -0700

    Added another project to projects

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -12,22 +12,14 @@ const Home = () => {
         AOS.init()
     }, [])
     return (
-        <div style={{display: 'flex', flexDirection: 'column', height: '100%' }}>
-            
+        <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+
             <Container className='home-container'>
+                <img style={{height: '250px', width: "150px"}} src="nathan's-photo.png" />
+                <span> Hi,<span style={{ fontSize: '50px' }} className='wave'>&#128075;</span>I'm <span style={{ color: '#3498db' }}>Nathan</span>.</span>
                 <h3 style={{ margin: '4rem auto' }}>
-                    Hi,<span style={{ fontSize: '50px' }} className='wave'>&#128075;</span>I'm <span style={{ color: '#3498db' }}>Nathan</span>. Welcome to my professional portfolio. I am a Software Developer with a passion for
-                    creating innovative and intuitive technology solutions. With a strong background in some of the
-                    most common programming languages, development frameworks and software design principles, I am
-                    confident in my ability to deliver high-quality, efficient code that meets the needs of my clients.
-                    My eagerness to learn new technologies and the willingness to work with a team makes me a valuable
-                    addition to any development project.
+                    Welcome to my professional portfolio. I am a Software Developer with a passion for creating innovative and intuitive technology solutions.
                 </h3>
-                <Button className='home-container-button'>
-                    <Link to='/about' style={{ textDecoration: 'none' }}>
-                        Learn More &gt;
-                    </Link>
-                </Button>
             </Container>
             <Container className='home-container'>
                 <div className='seperator-div'>
@@ -51,13 +43,20 @@ const Home = () => {
                     </div>
                     <h5 style={{ marginTop: '50px' }}>
                         Whether you are looking for a developer to join your team, or for someone to take on a small
-                        project, I am here to help bring your ideas to life. I am always looking for new challenges and
-                        opportunities to learn and grow as a developer. I invite you to take a look at my portfolio and see for
-                        yourself the quality of my work and my dedication to producing excellent software solutions.
+                        project, I am here to help bring your ideas to life. With a strong background in some of the
+                        most common programming languages, development frameworks and software design principles, I am
+                        confident in my ability to deliver high-quality, efficient code that meets the needs of my clients.
+                        My eagerness to learn new technologies and the willingness to work with a team makes me a valuable
+                        addition to any development project.
                     </h5>
                     <Button className='home-container-button'>
                         <Link to='/contact' style={{ textDecoration: 'none' }}>
                             Contact Me
+                        </Link>
+                    </Button>
+                    <Button className='home-container-button'>
+                        <Link to='/about' style={{ textDecoration: 'none' }}>
+                            Learn More &gt;
                         </Link>
                     </Button>
                 </div>
--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -12,10 +12,18 @@ const Projects = () => {
 
     const events = [
         {
+            name: "The-N-T Collection",
+            image: 'The-N-T-Collection-screenshot.png',
+            description: "This project aims to create a modern ecommerce application using ReactJS. Key features include displaying featured products, admin functionalities, and an intuitive cart display. Users can browse products, view details, and add items to the cart. The admin panel allows product and order management. The app will have responsive design, scalability, and optimal performance.",
+            skillsUsed: ["React", "Express", "Axios", "Node.js", "MongoDB", "JWT-Tokens"],
+            date: "June 2023",
+            github: 'https://github.com/NathanielGrandinette/The-N-T-Collection'
+        },
+        {
             name: "Productivity App",
             image: 'productivity.jpg',
             description: "This project is focused on developing a todo list app that provides a streamlined and intuitive interface for managing tasks and staying organized using React. We used Oauth2 to allow users to sign in using a google account as well as JSON web tokens and requireAuth middleware for user authentication and authorization. Tasks and notes are stored in a MongoDB database as well as account settings.",
-            skillsUsed: ["React", "Express", "Axios", "Node.js", "Oauth2", "JWT-Tokens"],
+            skillsUsed: ["React", "Express", "Axios", "MongoDB", "Node.js", "Oauth2", "JWT-Tokens"],
             date: "March 2023",
             github: 'https://github.com/TimothyDanfield/Agenda-X-Productivity',
             live: 'https://agendaxproductivity.herokuapp.com/'
