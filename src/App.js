4c91ee6c69b6e8470b53c1acc9326a7991d13ee8
Nathaniel Grandinette <nathaniel.grandinette@gmail.com>
2023-09-21 19:31:15 -0700

    Fixed google analytics

--- src/App.js
+++ src/App.js
@@ -1,46 +1,45 @@
-import './App.css';
-import NavBar from './components/Navbars/NavBar';
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/home/home'
-import Projects from './pages/projects/projects'
-import Contact from './pages/contact/contact'
-import About from './pages/about/about'
-import ReactGA from 'react-ga'
-import { useState } from 'react';
-
-
+import "./App.css";
+import NavBar from "./components/Navbars/NavBar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/home";
+import Projects from "./pages/projects/projects";
+import Contact from "./pages/contact/contact";
+import About from "./pages/about/about";
+import { useState } from "react";
 
 function App() {
-  const [tone, setTone] = useState('dark')
-  const [windows, setWindows] = useState(window.location.pathname.replace(/\//g,''))
-
-  ReactGA.initialize("G-1951T02CJ7")
-  ReactGA.set({ page: window.location.pathname })
-  ReactGA.pageview(window.location.pathname)
+  const [tone, setTone] = useState("dark");
+  const [windows, setWindows] = useState(
+    window.location.pathname.replace(/\//g, "")
+  );
 
   window.addEventListener("blur", () => {
-    document.title = "Come back... Nathaniel's Portfolio"
-  })
+    document.title = "Come back... Nathaniel's Portfolio";
+  });
 
   window.addEventListener("focus", () => {
-    document.title = "Nathaniel's Portfolio"
-  })
-  
-  return (
-    <div className="App"
-      style={{
-        backgroundColor: tone === 'light' ? 'white' : '#091927',
-        color: tone === 'light' ? 'black' : 'white'
-      }}>
-      <NavBar tone={tone} setTone={setTone} />
+    document.title = "Nathaniel's Portfolio";
+  });
 
-      <Routes>
-        <Route path='/' element={<Home tone={tone} />} />
-        <Route path='/projects' element={<Projects />} />
-        <Route path='/about' element={<About tone={tone} />} />
-        <Route path='/contact' element={<Contact tone={tone} />} />
-      </Routes>
-    </div>
+  return (
+    <>
+      <div
+        className="App"
+        style={{
+          backgroundColor: tone === "light" ? "white" : "#091927",
+          color: tone === "light" ? "black" : "white",
+        }}
+      >
+        <NavBar tone={tone} setTone={setTone} />
+
+        <Routes>
+          <Route path="/" element={<Home tone={tone} />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About tone={tone} />} />
+          <Route path="/contact" element={<Contact tone={tone} />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
